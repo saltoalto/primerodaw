915778c0379b66c77b0b88a733393a0b01ea0713
Usuario <Usuario@TI4-PROFE>
2020-09-25 12:58:05 +0200

    Versión 0

---
+++ src/first/Welcome.java
@@ -0,0 +1,12 @@
+package first;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello. My first program");
+		System.out.println();
+
+	}
+
+}
